5a0379235daf6cc6e88438807f1c1604060b8aaf
sayem <mahadisayem2001@gmail.com>
2024-04-17 02:03:30 -0400

    phase-2 code challenge done

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -3,9 +3,9 @@ import React from "react";
 function PlantCard({plant,sellPlant}) {
   return (
     <li className="card" data-testid="plant-item">
-      <img src={plant.image} alt={plant .name} />
-      <h4>{plant .name}</h4>
-      <p>Price: {plant. price}</p>
+      <img src={plant.image} alt={plant.name} />
+      <h4>{plant.name}</h4>
+      <p>Price: {plant.price}</p>
       {plant.sold? (
         <button className="primary" onClick={() => sellPlant (plant)}>In Stock</button>
       ) : (
